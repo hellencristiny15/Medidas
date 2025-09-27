426548132adc27aadd4694e4b90fe792c3861d6a
Hellen <cristinyhellen75@gmail.com>
2025-09-26 21:16:50 -0300

    atividade finalizada

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Entre com a medida (em metros)");
+double metros = Convert.ToDouble(Console.ReadLine());
+double centimetros = metros * 100;
+double quilometros = metros / 1000;
+Console.WriteLine("\nEquivalencia");
+Console.WriteLine($"{centimetros} cm");
+Console.WriteLine($"{metros} m");
+Console.WriteLine($"{quilometros} km");
